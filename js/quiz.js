48ca681ad497098657ef30271dbceef4f67c8c2c
jihye.kim <kim@j.hye>
2020-11-04 17:23:25 +0900

    QUIZ 메소드

--- js/quiz.js
+++ js/quiz.js
@@ -10,3 +10,33 @@ function addrInput(){
 function ageInput(){
 	age=prompt('당신의 나이는?')
 }
+
+
+
+imgList = ['최근상품01.jpg','최근상품02.jpg','최근상품03.jpg','최근상품04.jpg',
+'최근상품05.jpg','최근상품06.jpg','최근상품07.jpg','최근상품08.jpg']
+path = '../CSS/resources/images/'
+idx=0
+
+function previous(){
+		if(idx==0){
+			idx = 7
+		} else{
+			idx -=1;
+		}
+		
+		document.getElementById('img').src = path+imgList[idx]
+	}
+
+
+function next(){
+		
+		if(idx==7){
+			idx = 0
+		} else{
+			idx +=1;
+		}
+		
+		document.getElementById('img').src = path+imgList[idx]
+	}
+	
